e0bac141e4d2e6a144d2bac54c20471feae5f0cb
ziadpath <ziadma7moudd21@gmail.com>
2026-06-10 07:07:30 +0300

    feat: add multiply function

--- calc.py
+++ calc.py
@@ -7,3 +7,6 @@ def subtract(a, b):
 print("Calculator is ready!")
 print("5 + 3 =", add(5, 3))
 print("10 - 4 =", subtract(10, 4))
+def multiply(a, b):
+    return a * b
+print("4 x 5 =", multiply(4, 5))
